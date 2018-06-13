d1046c697e24e67f84a45a2e2540d07021f228b4
VicSz <vicsz>
2018-06-13 15:12:06 -0400

    Simplyfing design .. adding steps for Actuator.

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # Spring Boot PCF Demo
 
+## Initial Deployment
 To run application locally:
 
 ```sh
 ./mvnw spring-boot:run
 ```
+
+To create the binary:
+
+```sh
+./mvnw package
+```
+
+
+To push to pcf:
+
+```sh
+cf push pcf-demo -p target/pcf-demo-0.0.1-SNAPSHOT.jar --random-route
+```
+
+*The random route option is to ensure a route in-case it's already been taken.*
+
+Access your deployed application via the route generated.
+
+You will see corresponding PCF application information.
+
+## Spring Boot Actuator
+
+Check the */actuator* endpoint for available actuator URLS.
+
+Including: */info , /logfile , /env *
+
--- src/main/java/com/example/pcfdemo/WebController.java
+++ src/main/java/com/example/pcfdemo/WebController.java
@@ -1,28 +1,24 @@
 package com.example.pcfdemo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-@Controller
-@RequestMapping("/")
+@RestController
 public class WebController {
 
-    @RequestMapping(method= RequestMethod.GET)
-    public String hello(Locale locale, Model model) {
-        model.addAttribute("greeting", "Hello!");
+    @Value("${vcap.application.name:localMachine}")
+    private String applicationName;
 
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-        String formattedDate = dateFormat.format(date);
-        model.addAttribute("currentTime", formattedDate);
+    @Value("${vcap.application.space_name:localSpace}")
+    private String spaceName;
 
-        return "hello";
+
+    @Value("${vcap.application.instance_id:localInstanceId}")
+    private String instanceId;
+
+    @RequestMapping("/")
+    public String backendCall(){
+        return "PCF Info: " + applicationName + "@" + spaceName + " " + instanceId;
     }
-
 }
